cd7fe9874435c8e7a72def311038e628c09d2b80
Kaan <kaanasutay10@gmail.com>
2024-04-16 18:27:03 +0300

    firmRoutes finish

--- src/routes/firm.js
+++ src/routes/firm.js
@@ -4,3 +4,21 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+// routes/firm:
+
+const firm = require('../controllers/firm')
+
+// URL: /firms
+
+router.route('/')
+    .get(firm.list)
+    .post(firm.create)
+
+router.route('/:id')
+    .get(firm.read)
+    .put(firm.update)
+    .patch(firm.update)
+    .delete(firm.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
