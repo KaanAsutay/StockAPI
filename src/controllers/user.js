935c2ba49d97978a9ea1ae6630173b60636ef5be
Kaan <kaanasutay10@gmail.com>
2024-04-16 15:01:59 +0300

    userControllers list ok

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,3 +2,52 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// User Controller:
+
+const User = require('../models/user')
+
+module.exports = {
+
+    list: async (req, res) => {
+         /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+
+        const data = await res.getModellist(User)
+
+        // res.status(200).send({
+        //     error: false,
+        //     details: await res.getModelListDetails(User),
+        //     data
+        // })
+
+        // FOR REACT PROJECT:
+        res.status(200).send(data)
+    },
+
+    create: async (req, res) => {
+
+    },
+
+    read: async (req, res) => {
+
+    },
+
+    update: async (req, res) => {
+
+    },
+
+    delete: async (req, res) => {
+
+    },
+
+}
