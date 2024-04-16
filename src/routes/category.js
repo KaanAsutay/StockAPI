c092958d1b5c6a4326cc71901443383688490516
Kaan <kaanasutay10@gmail.com>
2024-04-16 18:25:12 +0300

    categoryRoutes finish

--- src/routes/category.js
+++ src/routes/category.js
@@ -4,3 +4,21 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+// routes/category:
+
+const category = require('../controllers/category')
+
+// URL: /categories
+
+router.route('/')
+    .get(category.list)
+    .post(category.create)
+
+router.route('/:id')
+    .get(category.read)
+    .put(category.update)
+    .patch(category.update)
+    .delete(category.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
