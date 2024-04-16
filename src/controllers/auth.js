418eb0da023bd04bdb7ad6e83ac7c4ed95abd622
Kaan <kaanasutay10@gmail.com>
2024-04-16 18:49:26 +0300

    Auth Controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,3 +2,8 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// Auth Controller:
+
+const User = require('../models/user')
+const Token = require('../models/token')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
