6a47c1bb4dbb4ae7ea088179c5602da0ac7e597c
Kaan <kaanasutay10@gmail.com>
2024-04-20 21:32:36 +0300

    add jwt

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -4,16 +4,25 @@
 ------------------------------------------------------- */
 // app.use(authentication):
 
+const jwt = require('jsonwebtoken')
 const Token = require('../models/token')
 
 module.exports = async (req, res, next) => {
 
-    const auth = req.headers?.authorization || null
-    const tokenKey = auth ? auth.split(' ') : null
+    const auth = req.headers?.authorization || null // Token ...tokenKey... // Bearer ...accessToken...
+    const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...'] // ['Bearer', '...accessToken...']
 
-    if (tokenKey && tokenKey[0] == 'Token') {
-        const tokenData = await Token.findOne({ token: tokenKey[1] }).populate('user_id')
-        req.user = tokenData ? tokenData.user_id : undefined
+    if (tokenKey) {
+
+        if (tokenKey[0] == 'Token') { // SimpleToken
+
+            const tokenData = await Token.findOne({ token: tokenKey[1] }).populate('user_id')
+            req.user = tokenData ? tokenData.user_id : undefined
+
+        } else if (tokenKey[0] == 'Bearer') { // JWT
+
+            jwt.verify(tokenKey[1], process.env.ACCESS_KEY, (err, userData) => req.user = userData)
+        }
     }
 
     next()
