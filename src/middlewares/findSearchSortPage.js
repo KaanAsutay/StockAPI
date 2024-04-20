c74a8ccb4d99a65308bc665421133f57fc452b63
Kaan <kaanasutay10@gmail.com>
2024-04-20 16:30:20 +0300

    add special code

--- src/middlewares/findSearchSortPage.js
+++ src/middlewares/findSearchSortPage.js
@@ -31,7 +31,9 @@ module.exports = (req, res, next) => {
 
         const filtersAndSearch = { ...filters, ...search  }
 
-        return await Model.find(filtersAndSearch).sort(sort).skip(skip).limit(limit).populate(populate)
+        // return await Model.find(filtersAndSearch).sort(sort).skip(skip).limit(limit).populate(populate)
+        // FOR REACT PROJECT:
+        return await Model.find(filtersAndSearch).populate(populate)
     }
 
     // Details:
