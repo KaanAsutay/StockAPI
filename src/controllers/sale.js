acdaac36ac28bd3ab796568853396849e9ef8ff7
Kaan <kaanasutay10@gmail.com>
2024-04-20 19:31:47 +0300

    delete and sale

--- src/controllers/sale.js
+++ src/controllers/sale.js
@@ -4,6 +4,7 @@
 ------------------------------------------------------- */
 // Sale Controller:
 
+const Product = require('../models/product')
 const Sale = require('../models/sale')
 
 module.exports = {
@@ -29,7 +30,7 @@ module.exports = {
         //     details: await res.getModelListDetails(Sale),
         //     data
         // })
-        
+
         // FOR REACT PROJECT:
         res.status(200).send(data)
     },
@@ -48,12 +49,27 @@ module.exports = {
         // Auto add user_id to req.body:
         req.body.user_id = req.user?._id
 
-        const data = await Sale.create(req.body)
+        // get stock info:
+        const currentProduct = await Product.findOne({ _id: req.body.product_id })
 
-        res.status(201).send({
-            error: false,
-            data
-        })
+        if (currentProduct.stock >= req.body.quantity) { // Check Limit
+
+            // Create:
+            const data = await Sale.create(req.body)
+
+            // set stock (quantity) when Sale process:
+            const updateProduct = await Product.updateOne({ _id: data.product_id }, { $inc: { stock: -data.quantity } })
+
+            res.status(201).send({
+                error: false,
+                data,
+            })
+
+        } else {
+
+            res.errorStatusCode = 422
+            throw new Error('There is not enough stock for this sale.', { cause: { currentProduct } })
+        }
     },
 
     read: async (req, res) => {
@@ -62,6 +78,7 @@ module.exports = {
             #swagger.summary = "Get Single Sale"
         */
 
+        // Read:
         const data = await Sale.findOne({ _id: req.params.id }).populate(['brand_id', 'product_id'])
 
         res.status(200).send({
@@ -81,6 +98,24 @@ module.exports = {
             }
         */
 
+        if (req.body?.quantity) {
+            // get current stock quantity from the Sale:
+            const currentSale = await Sale.findOne({ _id: req.params.id })
+            // different:
+            const quantity = req.body.quantity - currentSale.quantity
+            // console.log(quantity)
+            // set stock (quantity) when Sale process:
+            const updateProduct = await Product.updateOne({ _id: currentSale.product_id, stock: { $gte: quantity } }, { $inc: { stock: -quantity } })
+            // console.log(updateProduct)
+            
+            // if stock limit not enough:
+            if (updateProduct.modifiedCount == 0) { // Check Limit
+                res.errorStatusCode = 422
+                throw new Error('There is not enough stock for this sale.')
+            }
+        }
+
+        // Update:
         const data = await Sale.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
 
         res.status(202).send({
@@ -96,7 +131,15 @@ module.exports = {
             #swagger.summary = "Delete Sale"
         */
 
+        // get current stock quantity from the Sale:
+        const currentSale = await Sale.findOne({ _id: req.params.id })
+        // console.log(currentSale)
+
+        // Delete:
         const data = await Sale.deleteOne({ _id: req.params.id })
+
+        // set stock (quantity) when Sale process:
+        const updateProduct = await Product.updateOne({ _id: currentSale.product_id }, { $inc: { stock: +currentSale.quantity } })
 
         res.status(data.deletedCount ? 204 : 404).send({
             error: !data.deletedCount,
