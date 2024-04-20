1bf69fe7848a6a0dbac0019e4b40a8c27cd341d0
Kaan <kaanasutay10@gmail.com>
2024-04-20 19:23:25 +0300

    set stock (quantity) when Purchase process:

--- src/controllers/purchase.js
+++ src/controllers/purchase.js
@@ -86,6 +86,16 @@ module.exports = {
             }
         */
 
+        if (req.body?.quantity) {
+            // get current stock quantity from the Purchase:
+            const currentPurchase = await Purchase.findOne({ _id: req.params.id })
+            // different:
+            const quantity = req.body.quantity - currentPurchase.quantity
+            // set stock (quantity) when Purchase process:
+            const updateProduct = await Product.updateOne({ _id: currentPurchase.product_id }, { $inc: { stock: quantity } })
+
+        }
+
         const data = await Purchase.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
 
         res.status(202).send({
