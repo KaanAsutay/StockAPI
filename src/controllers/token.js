8a3967397ac574bf93baef71b26fe01157bbf812
Kaan <kaanasutay10@gmail.com>
2024-04-20 21:40:18 +0300

    clear code

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -4,52 +4,30 @@
 ------------------------------------------------------- */
 // Token Controller:
 
-const Token = require('../models/user')
+const Token = require('../models/token')
 
 module.exports = {
 
     list: async (req, res) => {
-         /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "List Tokens"
-            #swagger.description = `
-                You can send query with endpoint for search[], sort[], page and limit.
-                <ul> Examples:
-                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
-                    <li>URL/?<b>page=2&limit=1</b></li>
-                </ul>
-            `
+        /*
+            #swagger.ignore = true
         */
-
 
         const data = await res.getModelList(Token)
 
-        res.status(200).send({
-            error: false,
-            details: await res.getModelListDetails(Token),
-            data
-        })
-
+        // res.status(200).send({
+        //     error: false,
+        //     details: await res.getModelListDetails(Token),
+        //     data
+        // })
+        
         // FOR REACT PROJECT:
-        // res.status(200).send(data)
+        res.status(200).send(data)
     },
 
     create: async (req, res) => {
-         /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Create Token"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    "username": "test",
-                    "password": "1234",
-                    "email": "test@site.com",
-                    "first_name": "test",
-                    "last_name": "test",
-                }
-            }
+        /*
+            #swagger.ignore = true
         */
 
         const data = await Token.create(req.body)
@@ -58,14 +36,11 @@ module.exports = {
             error: false,
             data
         })
-
-
     },
 
     read: async (req, res) => {
         /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Get Single Token"
+            #swagger.ignore = true
         */
 
         const data = await Token.findOne({ _id: req.params.id })
@@ -74,24 +49,11 @@ module.exports = {
             error: false,
             data
         })
-
     },
 
     update: async (req, res) => {
         /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Update Token"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    "username": "test",
-                    "password": "1234",
-                    "email": "test@site.com",
-                    "first_name": "test",
-                    "last_name": "test",
-                }
-            }
+            #swagger.ignore = true
         */
 
         const data = await Token.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
@@ -105,8 +67,7 @@ module.exports = {
 
     delete: async (req, res) => {
         /*
-            #swagger.tags = ["Tokens"]
-            #swagger.summary = "Delete Token"
+            #swagger.ignore = true
         */
 
         const data = await Token.deleteOne({ _id: req.params.id })
@@ -115,7 +76,5 @@ module.exports = {
             error: !data.deletedCount,
             data
         })
-
     },
-
 }
