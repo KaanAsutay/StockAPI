1b7d0c83a197c810ca6f6d644cff80f8c7c19826
Kaan <kaanasutay10@gmail.com>
2024-04-21 21:21:10 +0300

    add fs.mkdirSync

--- backend/src/middlewares/logger.js
+++ backend/src/middlewares/logger.js
@@ -8,6 +8,8 @@
 const morgan = require('morgan')
 const fs = require('node:fs')
 
+// fs.mkdirSync('./logs', { recursive: true })
+
 const now = new Date()
 const today = now.toISOString().split('T')[0]
 
